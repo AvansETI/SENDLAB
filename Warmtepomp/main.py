065b926ae4c463d1c2fbe66c6a7a34df1037370a
Ties Tienhoven <tiestienhoven@gmail.com>
2021-05-27 14:04:24 +0200

    Update main.py

--- Warmtepomp/main.py
+++ Warmtepomp/main.py
@@ -5,10 +5,11 @@ sendlab = mqtt.Client()
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
+    print(client.hostname)
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("#")
+    # client.subscribe("#")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
@@ -18,9 +19,14 @@ def on_message(client, userdata, msg):
 localhost.on_connect = on_connect
 localhost.on_message = on_message
 
+sendlab.on_connect = on_connect
+sendlab.on_message = on_message
+
 localhost.connect("localhost", 1883, 60)
-# sendlab.username_pw_set("server", password="servernode")
-# sendlab.connect("sendlab.nl", 11884, 60)
+
+sendlab.username_pw_set("server", password="servernode")
+sendlab.connect("sendlab.nl", 11884, 60)
 
 while( 1 ):
     localhost.loop()
+    sendlab.loop()
