0a5f62d80353735c6c6c0e24cb337ce0ccb82b12
Ties Tienhoven <tiestienhoven@gmail.com>
2021-05-27 13:18:29 +0200

    Update main.py

--- Warmtepomp/main.py
+++ Warmtepomp/main.py
@@ -3,7 +3,7 @@ import paho.mqtt.client as mqtt
 localhost = mqtt.Client("heatpump", transport="websockets")
 sendlab = mqtt.Client()
 
-def on_connect_localhost(client, userdata, flags, rc):
+def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
@@ -11,11 +11,12 @@ def on_connect_localhost(client, userdata, flags, rc):
     client.subscribe("$SYS/#")
 
 # The callback for when a PUBLISH message is received from the server.
-def on_message_localhost(client, userdata, msg):
+def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
 
 
-localhost.on_connect = on_connect_localhost
-localhost.on_message = on_message_localhost
+sendlab.on_connect = on_connect
+sendlab.on_message = on_message
 
-localhost.connect("localhost", 1183, 60, "10.0.0.1")
+#localhost.connect("localhost", 1183, 60, "10.0.0.1")
+sendlab.connect("sendlab.nl", 11884, 60)
