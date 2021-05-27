bb5670e2810c0a29918c2aa0409010bd9e16ace7
Ties Tienhoven <tiestienhoven@gmail.com>
2021-05-27 10:32:44 +0200

    Small script to test mqtt

---
+++ Warmtepomp/main.py
@@ -0,0 +1,21 @@
+import paho.mqtt.client as mqtt
+
+localhost = mqtt.Client()
+sendlab = mqtt.Client()
+
+def on_connect_localhost(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("$SYS/#")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message_localhost(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+
+localhost.on_connect = on_connect_localhost
+localhost.on_message = on_message_localhost
+
+localhost.connect("localhost", 1183, 60)
