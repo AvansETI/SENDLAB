2902620e04e0e56758f0b65ddd829b389be656f0
Maurice Snoeren <mac.snoeren@avans.nl>
2022-07-05 08:55:34 +0200

    Set simulation to false

--- RemoteLabs/HostPC/hostpc.py
+++ RemoteLabs/HostPC/hostpc.py
@@ -28,7 +28,7 @@ sio = socketio.Client(logger=True, engineio_logger=True, ssl_verify=False)
 
 hostpc = RemoteLabsHostPC(sio)
 hostpc.debug = True
-hostpc.simulation = True
+hostpc.simulation = False
 hostpc.start()
 
 @sio.event
